21676098a8290c6c84b1896815576b025dc181a8
stoega <fs.stoeger2@gmail.com>
2023-11-08 07:16:47 +0100

    typo

--- a3/a3_ex1.py
+++ a3/a3_ex1.py
@@ -6,8 +6,8 @@ matrix = [[0 for col in range(cols)] for row in range(rows)]
 for i in range(min(rows, cols)):
     matrix[i][i] = 1
         
-print(" " * 3 + " ".join(str(col) for col in range(cols))) #" ".join(map(str, range(cols)))
+print(" " * 3 + " ".join(str(col) for col in range(cols)))
 print(" " * 2 + "-" * cols * 2)
 for row in range(rows):
-    print(f"{row}| " + " ".join(str(col) for col in matrix[row])) #" ".join(map(str, matrix[row]))
+    print(f"{row}| " + " ".join(str(col) for col in matrix[row]))
     
